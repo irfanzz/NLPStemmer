d52adff8bee56686463ead6b3fcece447940cb8f
Hasan <achank89@gmail.com>
2015-02-12 23:00:57 +0700

    Update dokumentasi

--- NLPStemming/src/nlpstemming/mainTest.java
+++ NLPStemming/src/nlpstemming/mainTest.java
@@ -21,14 +21,6 @@ public class mainTest {
         ArrayDictionary dictionary = new ArrayDictionary(words);
         Stemmer stemmer    = new Stemmer(dictionary);
 
-        System.out.println(stemmer.stem("Algoritma stemming kata pada Bahasa Indonesia dengan performa "
-                + "yang paling baik memiliki jenis kesalahan stemming yang paling sedikit adalah "
-                + "algoritma Enhanced Confix Stripping ECS Stemmer Meskipun terdapat peningkatan "
-                + "performa stemming kata masih terdapat kesalahan yang dilakukan oleh ECS Stemmer "
-                + "Selain itu algoritma ECS Stemmer juga tidak mengajukan perbaikan terhadap "
-                + "permasalahan overstemming dan understemming Dalam tugas akhir ini diajukan "
-                + "perbaikan terhadap algoritma ECS Stemmer Selain perbaikan terhadap aturan pada "
-                + "tabel acuan pemenggalan imbuhan juga dilakukan implementasi metode corpus based stemming untuk melakukan penyelesaian terhadap problem overstemming dan understemmingProses evaluasi sistem Information Retrieval IR menggunakan relevansi set dokumen terhadap query yang dibentuk secara otomatis menggunakan teknik data fusion dan metode condorcet Karena tidak dibentuk secara manual relevan set tersebut dinamakan pseudo relevant documents pseudorels"));
-        System.out.println(stemmer.stem("Budi belajar membaca dan menulis dengan buku tulis di sekolah"));
+       System.out.println(stemmer.stem("Apakah"));
     }
 }
